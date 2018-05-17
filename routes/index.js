b5f9ada1375ef18dbfe72c075be0e6d2c017404a
TorreJohnson <torre.johnson@gmail.com>
2018-05-16 22:22:55 -0400

    added create route/view and post route

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,25 @@ router.get("/newuser", function(req, res) {
 	res.render("newuser", { title: "Add New User" });
 });
 
+/* POST to Add User */
+router.post("/adduser", function(req, res) {
+	var db = req.db;
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
+	var collection = db.get("usercollection");
+	collection.insert(
+		{
+			username: userName,
+			email: userEmail
+		},
+		function(err, doc) {
+			if (err) {
+				res.send("There was a problem adding the information to the database.");
+			} else {
+				res.redirect("userlist");
+			}
+		}
+	);
+});
+
 module.exports = router;
